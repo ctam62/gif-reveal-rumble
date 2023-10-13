6c36f611652031f2c82bea295abe73c7625a2e55
claratam <claramtam@gmail.com>
2023-10-13 05:10:41 -0500

    create card flip and card match features

--- scripts/game.js
+++ scripts/game.js
@@ -5,25 +5,74 @@ function displayCards(gifData) {
     const cardRow1 = createElementsWithClasses("div", ["game__card-row"]);
     const cardRow2 = createElementsWithClasses("div", ["game__card-row"]);
 
-    // Create random card indexArray to assign gifs to
     const numGifs = Object.keys(gifData).length;
+
+    // Create card randomIndexArray
     const randIndexArray = shuffle(randomPairedArray(0, numGifs));
 
-    console.log(shuffle(randomPairedArray(0, numGifs)));
-    console.log(Object.keys(gifData).length);
-
-    //check if easy, medium, hard level
+    // Assign Gifs to randomly to cards
     randIndexArray.forEach((value, index) => {
         const cardBody = createCards(gifData, value);
         let cardRow = cardRow1;
-        if (index <= 2) {
+        if (index <= numGifs - 1) {
             cardRow1.appendChild(cardBody);
         } else {
             cardRow2.appendChild(cardBody);
             cardRow = cardRow2;
         }
-        if ((index + 1) % 3 === 0) {
+        if ((index + 1) % numGifs === 0) {
             gameContent.appendChild(cardRow);
+        }
+    });
+
+
+    const cardsContainer = document.querySelector(".game__content");
+    let clickCounter = 0;
+    let matchCounter = 0;
+
+    // Click event to flip cards
+    cardsContainer.addEventListener("click", (event) => {
+        event.preventDefault();
+        clickCounter += 1;
+        // Identify closest card when clicked event occurred
+        const closestCard = event.target.closest(".game__card");
+
+        // Identify the already selected card
+        const activeCard = event.currentTarget.querySelector(".game__card--selected");
+        const activeCardsLength = document.querySelectorAll(".game__card--selected").length;
+
+        console.log("click counter:", clickCounter);
+        // Add selected class to clicked card
+        closestCard.classList.add("game__card--selected");
+        closestCard.id = clickCounter;
+
+        // Remove selected class from previously clicked card
+        // do this only if second clicked card does not match first card
+        if (clickCounter === 2) {
+            clickCounter = 0;
+            const previousCard = document.getElementById("1");
+            const previousIframe = previousCard.childNodes[0].childNodes[0].childNodes[0];
+            const currentCard = document.getElementById("2");
+            const activeIframe = currentCard.childNodes[0].childNodes[0].childNodes[0];
+
+            if (activeIframe.src !== previousIframe.src) {
+                setTimeout(() => {
+                    removeSelectedClass();
+                }, 300);
+            } else {
+                matchCounter += 1;
+                previousCard.classList.add("game__card--matched");
+                currentCard.classList.add("game__card--matched");
+                console.log("match counter:", matchCounter);
+                const score = document.querySelector(".game__score-counter");
+                score.textContent = `${Number(score.textContent) + 5}`
+            }
+        }
+
+        if (matchCounter === numGifs) {
+            // update score
+            const score = document.querySelector(".game__score-counter");
+            score.textContent = `${Number(score.textContent) + 5}`
         }
     });
 }
@@ -69,8 +118,6 @@ function randomPairedArray(min, max) {
     return pairedArray;
 }
 
-randomPairedArray(0, 3);
-
 function shuffle(array) {
     let count = array.length, temp, index;
 
@@ -88,3 +135,10 @@ function shuffle(array) {
     return array;
 }
 
+function removeSelectedClass() {
+    const activeCards = document.querySelectorAll(".game__card--selected");
+    activeCards.forEach((card) => {
+        card.classList.remove("game__card--selected");
+        card.id = 0;
+    });
+};
