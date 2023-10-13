c741269caee285a8fb594229a041e3d1de137e2f
claratam <claramtam@gmail.com>
2023-10-13 13:22:30 -0500

    add random shuffle of gif images pulled from giphy api

--- scripts/giphy-api.js
+++ scripts/giphy-api.js
@@ -6,10 +6,11 @@ class GiphyApi {
         this.baseURL = "https://api.giphy.com/v1/gifs/search";
     }
 
-    getGifs(limit) {
+    getGifs(search, limit) {
+        console.log(search);
         const fetchGifs = async () => {
             try {
-                const response = await axios.get(`${this.baseURL}?api_key=${this.apiKey}&q=happy&limit=${limit}&offset=0&rating=pg-13&lang=en&bundle=messaging_non_clips`);
+                const response = await axios.get(`${this.baseURL}?api_key=${this.apiKey}&q=${search}&limit=${limit}&offset=0&rating=pg-13&lang=en&bundle=messaging_non_clips`);
                 const gifData = response.data.data;
                 console.log(gifData);
                 displayCards(gifData);
@@ -26,12 +27,13 @@ const gifApi = new GiphyApi(apiKey);
 
 const level = document.querySelector(".game__level");
 
+const searchTerms = ["happy", "excited", "sad", "laughing"];
+const shuffledArray = shuffleArray(searchTerms);
 // Check if difficulty level
 if (level.textContent.toLowerCase() === "medium") {
-    gifApi.getGifs(4);
+    gifApi.getGifs(shuffledArray[0], 4);
 } else if (level.textContent.toLowerCase() === "hard") {
-    gifApi.getGifs(6);
+    gifApi.getGifs(shuffledArray[0], 6);
 } else {
-    gifApi.getGifs(3);
+    gifApi.getGifs(shuffledArray[0], 3);
 }
-
